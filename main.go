e777c77d803c026c4f7d9a86ed7163590877cf6e
mductran <ductran032118@gmail.com>
2024-04-12 23:19:49 +0700

    switching to channel for output

--- main.go
+++ main.go
@@ -41,12 +41,6 @@ type apiGateway struct {
 	Regions   []string
 }
 
-type ApiGateway interface {
-	Initialize()
-	Start()
-	Reroute()
-}
-
 func randomIpv4() net.IP {
 	buf := make([]byte, 4)
 	ip := rand.Uint32()
@@ -83,10 +77,7 @@ func ApiExistsInRegion(client *apigateway.Client, name string, region string) bo
 }
 
 // initializes the API Gateway in the specified region.
-func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
-
-	fmt.Println("initializing")
-
+func (ag *apiGateway) Initialize(region string, errChan chan error) {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		panic(err)
@@ -95,11 +86,12 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 	client := apigateway.NewFromConfig(cfg)
 
 	if ApiExistsInRegion(client, ag.Name, region) {
-		return fmt.Errorf("an API already exists with name: %s in region %s", ag.Name, region)
+		errChan <- fmt.Errorf("an API already exists with name: %s in region %s", ag.Name, region)
+		return
 	}
 
 	// create new REST API
-	newApi, err := client.CreateRestApi(ctx, &apigateway.CreateRestApiInput{
+	newApi, err := client.CreateRestApi(context.TODO(), &apigateway.CreateRestApiInput{
 		Name: &ag.Name,
 		EndpointConfiguration: &types.EndpointConfiguration{
 			Types: []types.EndpointType{
@@ -108,7 +100,8 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		},
 	})
 	if err != nil {
-		return fmt.Errorf("cannot create new API: %w", err)
+		errChan <- fmt.Errorf("cannot create new API: %w", err)
+		return
 	}
 
 	allowedHttpMethod := "ANY"
@@ -118,7 +111,7 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 	params["method.request.header.X-Forwarded-For-Temp"] = true // preserve X-Forwarded-For header by using a temp header X-My-X-Fowarded-For
 
 	// allow all methods to new resource
-	_, err = client.PutMethod(ctx, &apigateway.PutMethodInput{
+	_, err = client.PutMethod(context.TODO(), &apigateway.PutMethodInput{
 		RestApiId:         newApi.Id,
 		ResourceId:        newApi.RootResourceId,
 		HttpMethod:        &allowedHttpMethod,
@@ -126,14 +119,15 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		RequestParameters: params,
 	})
 	if err != nil {
-		return fmt.Errorf("cannot create method: %w", err)
+		errChan <- fmt.Errorf("cannot create method: %w", err)
+		return
 	}
 
 	// make new resource route traffic to new host
 	integrationParams := make(map[string]string)
 	integrationParams["integration.request.path.proxy"] = "method.request.path.proxy"
 	integrationParams["integration.request.header.X-Forwarded-For"] = "method.request.header.X-Forwarded-For-Temp"
-	_, err = client.PutIntegration(ctx, &apigateway.PutIntegrationInput{
+	_, err = client.PutIntegration(context.TODO(), &apigateway.PutIntegrationInput{
 		RestApiId:             newApi.Id,
 		ResourceId:            newApi.RootResourceId,
 		Type:                  types.IntegrationTypeHttpProxy,
@@ -144,21 +138,23 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		RequestParameters:     integrationParams,
 	})
 	if err != nil {
-		return fmt.Errorf("cannot create integration: %w", err)
+		errChan <- fmt.Errorf("cannot create integration: %w", err)
+		return
 	}
 
 	wildcardPath := "{proxy+}"
-	wildcardHandler, err := client.CreateResource(ctx, &apigateway.CreateResourceInput{
+	wildcardHandler, err := client.CreateResource(context.TODO(), &apigateway.CreateResourceInput{
 		RestApiId: newApi.Id,
 		ParentId:  newApi.RootResourceId,
 		PathPart:  &wildcardPath,
 	})
 	if err != nil {
-		return fmt.Errorf("cannot create wildcard handler: %w", err)
+		errChan <- fmt.Errorf("cannot create wildcard handler: %w", err)
+		return
 	}
 
 	// handle requests received for the wildcard handler
-	_, err = client.PutMethod(ctx, &apigateway.PutMethodInput{
+	_, err = client.PutMethod(context.TODO(), &apigateway.PutMethodInput{
 		RestApiId:         newApi.Id,
 		ResourceId:        wildcardHandler.Id,
 		HttpMethod:        &allowedHttpMethod,
@@ -166,10 +162,11 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		RequestParameters: params,
 	})
 	if err != nil {
-		return fmt.Errorf("cannot create wildcard method input: %w", err)
+		errChan <- fmt.Errorf("cannot create wildcard method input: %w", err)
+		return
 	}
 
-	_, err = client.PutIntegration(ctx, &apigateway.PutIntegrationInput{
+	_, err = client.PutIntegration(context.TODO(), &apigateway.PutIntegrationInput{
 		RestApiId:             newApi.Id,
 		ResourceId:            wildcardHandler.Id,
 		Type:                  types.IntegrationTypeHttpProxy,
@@ -180,7 +177,8 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		RequestParameters:     integrationParams,
 	})
 	if err != nil {
-		return fmt.Errorf("cannot integrate wildcard method: %w", err)
+		errChan <- fmt.Errorf("cannot integrate wildcard method: %w", err)
+		return
 	}
 
 	// create deployment resource so the new API is callable
@@ -190,40 +188,8 @@ func (ag *apiGateway) Initialize(region string, ctx context.Context) error {
 		StageName: &stageName,
 	})
 	if err != nil {
-		return err
+		errChan <- err
 	}
-
-	return nil
-}
-
-// route the original request through a proxy
-func (ag *apiGateway) Reroute(request *http.Request) {
-	// use a random endpoints as proxy
-	ep := ag.Endpoints[rand.Intn(len(ag.Endpoints)-1)]
-
-	// replace request's url with proxy endpoint and replace Host header
-	_, site, found := strings.Cut(request.RequestURI, "://")
-	if !found {
-		return
-	}
-
-	proxyUrl, err := url.Parse("https://" + ep + "/ProxyStage" + site)
-	if err != nil {
-		return
-	}
-	request.URL = proxyUrl
-	request.Header.Add("Host", ep)
-
-	// generate X-Forwarded-For header if original request does not have it
-	// and move original X-Forwarded-For to a temp header
-	val := request.Header.Get("X-Forwarded-For")
-	if val == "" {
-		randIp := randomIpv4().String()
-		request.Header.Add("X-Forwarded-For-Temp", randIp)
-	} else {
-		request.Header.Add("X-Forwarded-For-Temp", val)
-	}
-	request.Header.Del("X-Forwarded-For")
 }
 
 func (ag *apiGateway) GetGateways(region string, ctx context.Context) (*[]types.RestApi, error) {
@@ -237,7 +203,6 @@ func (ag *apiGateway) GetGateways(region string, ctx context.Context) (*[]types.
 		panic(err)
 	}
 	cfg.Region = region
-	fmt.Println(cfg.Region)
 	client := apigateway.NewFromConfig(cfg)
 
 	for !complete {
@@ -307,17 +272,60 @@ func (ag *apiGateway) DeleteGateways(region string, ctx context.Context) (*[]str
 	return &deletedIds, nil
 }
 
+// route the original request through a proxy
+func (ag *apiGateway) Reroute(request *http.Request) {
+	// use a random endpoints as proxy
+	ep := ag.Endpoints[rand.Intn(len(ag.Endpoints)-1)]
+
+	// replace request's url with proxy endpoint and replace Host header
+	_, site, found := strings.Cut(request.RequestURI, "://")
+	if !found {
+		return
+	}
+
+	proxyUrl, err := url.Parse("https://" + ep + "/ProxyStage" + site)
+	if err != nil {
+		return
+	}
+	request.URL = proxyUrl
+	request.Header.Add("Host", ep)
+
+	// generate X-Forwarded-For header if original request does not have it
+	// and move original X-Forwarded-For to a temp header
+	val := request.Header.Get("X-Forwarded-For")
+	if val == "" {
+		randIp := randomIpv4().String()
+		request.Header.Add("X-Forwarded-For-Temp", randIp)
+	} else {
+		request.Header.Add("X-Forwarded-For-Temp", val)
+	}
+	request.Header.Del("X-Forwarded-For")
+}
+
 func main() {
 	gateway, err := NewApiGateway("https://mangahere.cc", "mangahere")
 	if err != nil {
 		panic(err)
 	}
 
+	endpointsChan := make(chan string)
+	// idsChan := make(chan string)
+	errsChan := make(chan error)
+
+	for _, re := range DEFAULT_REGIONS {
+		go gateway.Initialize(re, errsChan)
+	}
+
+	fmt.Println("done")
+
 	for _, re := range DEFAULT_REGIONS {
 		endpoints, err := gateway.GetEndpoints(re, context.TODO())
 		if err != nil {
-			fmt.Println(err)
+			errsChan <- err
+		} else {
+			for _, ep := range *endpoints {
+				endpointsChan <- ep
+			}
 		}
-		fmt.Println(endpoints)
 	}
 }
